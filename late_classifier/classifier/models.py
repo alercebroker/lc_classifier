f802c76eaad86b88f29b752c9bf0c539a0eee5bc
Ignacio Reyes <ireyesj93@gmail.com>
2020-02-10 21:22:45 -0300

    Hierarchical classifier is being implemented. Some methods are pending, nothing has been tested

--- late_classifier/classifier/models.py
+++ late_classifier/classifier/models.py
@@ -2,9 +2,32 @@ import pandas as pd
 from imblearn.ensemble import BalancedRandomForestClassifier as RandomForestClassifier
 from late_classifier.classifier.preprocessing import FeaturePreprocessor
 from late_classifier.classifier.preprocessing import intersect_oids_in_dataframes
+from abc import ABC, abstractmethod
 
 
-class BaselineRandomForest:
+class BaseClassifier(ABC):
+    @abstractmethod
+    def fit(self, samples: pd.DataFrame, labels: pd.DataFrame) -> None:
+        pass
+
+    @abstractmethod
+    def predict(self, samples: pd.DataFrame) -> pd.DataFrame:
+        pass
+
+    @abstractmethod
+    def predict_proba(self, samples: pd.DataFrame) -> pd.DataFrame:
+        pass
+
+
+def invert_dictionary(dictionary):
+    inverted_dictionary = {}
+    for top_group, list_of_classes in dictionary:
+        for astro_class in list_of_classes:
+            inverted_dictionary[astro_class] = top_group
+    return inverted_dictionary
+
+
+class BaselineRandomForest(BaseClassifier):
     def __init__(self):
         self.random_forest_classifier = RandomForestClassifier(
             n_estimators=500,
@@ -56,3 +79,83 @@ class BaselineRandomForest:
 
     def get_list_of_classes(self) -> list:
         return self.random_forest_classifier.classes_
+
+
+class HierarchicalRandomForest(BaseClassifier):
+    def __init__(self, taxonomy_dictionary):
+        n_trees = 500
+        self.top_classifier = RandomForestClassifier(
+            n_estimators=n_trees,
+            max_depth=None,
+            max_features='auto'
+        )
+
+        self.stochastic_classifier = RandomForestClassifier(
+            n_estimators=n_trees,
+            max_depth=None,
+            max_features=0.2
+        )
+
+        self.periodic_classifier = RandomForestClassifier(
+            n_estimators=n_trees,
+            max_depth=None,
+            max_features='auto'
+        )
+
+        self.transient_classifier = RandomForestClassifier(
+            n_estimators=n_trees,
+            max_depth=None,
+            max_features='auto'
+        )
+
+        self.feature_preprocessor = FeaturePreprocessor()
+
+        self.taxonomy_dictionary = taxonomy_dictionary
+        self.inverted_dictionary = invert_dictionary(self.taxonomy_dictionary)
+
+    def fit(self, samples: pd.DataFrame, labels: pd.DataFrame) -> None:
+        labels = labels.copy()
+
+        # Check that the received labels are in the taxonomy
+        feeded_labels = labels.classALeRCE.unique()
+        expected_labels = self.inverted_dictionary.keys()
+
+        for label in feeded_labels:
+            if label not in expected_labels:
+                raise Exception(f'{label} is not in the taxonomy dictionary')
+
+        # Create top class
+        labels['top_class'] = labels['classALeRCE'].map(self.inverted_dictionary)
+
+        # Preprocessing
+        samples = self.feature_preprocessor.preprocess_features(samples)
+        samples = self.feature_preprocessor.remove_duplicates(samples)
+        samples, labels = intersect_oids_in_dataframes(samples, labels)
+
+        # Train top classifier
+        self.top_classifier.fit(samples.values, labels['top_class'].values)
+
+        # Train specialized classifiers
+        is_stochastic = labels['top_class'] == 'stochastic'
+        self.stochastic_classifier.fit(
+            samples[is_stochastic].values,
+            labels[is_stochastic]['classALeRCE'].values
+        )
+
+        is_periodic = labels['top_class'] == 'periodic'
+        self.periodic_classifier.fit(
+            samples[is_periodic].values,
+            labels[is_periodic]['classALeRCE'].values
+        )
+
+        is_transient = labels['top_class'] == 'transient'
+        self.transient_classifier.fit(
+            samples[is_transient].values,
+            labels[is_transient]['classALeRCE'].values
+        )
+
+    def predict(self, samples: pd.DataFrame) -> pd.DataFrame:
+        pass
+
+    def predict_proba(self, samples: pd.DataFrame) -> pd.DataFrame:
+        pass
