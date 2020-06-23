ae065841f927ba1a1fe2115124391daf5c5ae24d
Paula Sanchez <pasanchezsaez@gmail.com>
2020-06-23 17:04:39 -0400

    The corrected column in table objects is used to determine which columns use for mag and magerr

--- late_classifier/features/extractors/color_feature_extractor.py
+++ late_classifier/features/extractors/color_feature_extractor.py
@@ -19,21 +19,37 @@ class ColorFeatureExtractor(FeatureExtractor):
         detections :class:pandas.`DataFrame`
         DataFrame with detections of an object.
 
-        kwargs Not required.
+        objects : class:pandas.`DataFrame`
+        Dataframe with the objects table.
 
         Returns :class:pandas.`DataFrame`
         -------
         """
+
+        required = ['objects']
+        for key in required:
+            if key not in kwargs:
+                raise Exception(f'MHPSExtractor requires {key} argument')
+
+        objects = kwargs['objects']
+
+
         # pd.options.display.precision = 10
         oids = detections.index.unique()
         colors = []
         for oid in oids:
             oid_detections = detections.loc[[oid]]
+            oid_objects = magstats.loc[[oid]]
+
             if 1 not in oid_detections.fid.unique() or 2 not in oid_detections.fid.unique():
                 logging.info(
                     f'extractor=COLOR  object={oid}  required_cols={self.get_required_keys()}  filters_qty=2')
                 colors.append(self.nan_df(oid))
                 continue
+
+            objects_corrected = oid_objects.corrected
+
+
 
             g_band_mag_corr = oid_detections[oid_detections.fid == 1]['magpsf_corr'].values
             r_band_mag_corr = oid_detections[oid_detections.fid == 2]['magpsf_corr'].values
@@ -44,8 +60,13 @@ class ColorFeatureExtractor(FeatureExtractor):
             g_r_max = g_band_mag.min() - r_band_mag.min()
             g_r_mean = g_band_mag.mean() - r_band_mag.mean()
 
-            g_r_max_corr = g_band_mag_corr.min() - r_band_mag_corr.min()
-            g_r_mean_corr = g_band_mag_corr.mean() - r_band_mag_corr.mean()
+            if objects_corrected:
+                g_r_max_corr = g_band_mag_corr.min() - r_band_mag_corr.min()
+                g_r_mean_corr = g_band_mag_corr.mean() - r_band_mag_corr.mean()
+
+            else:
+                g_r_max_corr = np.nan
+                g_r_mean_corr = np.nan
 
             data = [g_r_max, g_r_mean, g_r_max_corr, g_r_mean_corr]
             oid_color = pd.DataFrame([data], columns=self.get_features_keys(), index=[oid])
