a3354700706c92e8d799fe9bb346974189fa2618
Ignacio Reyes <ireyesj93@gmail.com>
2021-03-05 01:50:29 -0300

    New extractor for forced photometry without non-detections

--- lc_classifier/features/extractors/sn_detections_extractor.py
+++ lc_classifier/features/extractors/sn_detections_extractor.py
@@ -41,6 +41,7 @@ class SupernovaeDetectionFeatureExtractor(FeatureExtractorSingleBand):
 
         def aux_function(oid_detections, **kwargs):
             if band not in oid_detections.fid.values:
+                oid = oid_detections.index.values[0]
                 logging.debug(
                     f'extractor=SN detection object={oid} required_cols={self.get_required_keys()} band={band}')
                 return self.nan_series_in_band(band)
